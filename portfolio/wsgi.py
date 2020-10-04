13374c9dd7527471b895748a3ab0539704930698
Rahul <70948208+rahul4497@users.noreply.github.com>
2020-10-04 12:33:54 +0530

    Update wsgi.py
    Added WSGI for AWS change

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+
+#application = get_wsgi_application()
+
 import os
-
+import sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/Django-mysite')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/Django-mysite/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django-mysite.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
 application = get_wsgi_application()
